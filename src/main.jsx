750cdf12122438e8cb282cd782cc5c723e87ecbc
peda <fedepedano2003@gmail.com>
2025-03-10 15:49:54 -0300

    Mejoras, finalizando carousel

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {Nav} from './nav.jsx'
 import Header from './header.jsx'
 import Carousel from './carousel.jsx'
+import TecnologiasDiapositiva from './tecnologias.jsx'
 
 const root=createRoot(document.getElementById('root'))
 //diapositivas que va a usar el componente Carousel
@@ -12,8 +12,7 @@ const slides = [
   <div id='presentacion' className="flex-col h-full justify-items-center w-full">
   <h2 className='font-bold p-10 underline decoration-sky-700 md:text-3xl'>QUIENES SOMOS</h2>
   <div className='w-full h-full md:flex md:justify-between p-20'>
-     <p className='font-serif md:w-1/2 w-full md:text-2xl text-sm mb-3'>Somos una empresa dedicada a la creación de tecnología de punta, con el objetivo de mejorar la calidad de vida de las personas.
-     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, debitis numquam blanditiis quisquam quaerat voluptate et voluptas eveniet molestias fuga eius! Reiciendis fugit, laudantium ipsa fugiat repudiandae quaerat quod dolor!
+     <p className='font-sans md:w-1/2 w-full md:text-2xl text-base mb-6'>Somos una empresa dedicada a la creación de tecnología de punta. Diseñamos plataformas digitales innovadoras que facilitan el acceso a la educación y la información, con el objetivo de mejorar la calidad de vida de las personas, empoderándolas para alcanzar su máximo potencial.
      </p>
      <div className="tenor-gif-embed md:h-1/2 md:w-1/4 w-full p-10" data-postid="9466370892182877823" data-share-method="host" data-aspect-ratio="1">
        <a href="https://tenor.com/view/carregando-loading-gif-9466370892182877823">Carregando Loading GIF</a>
@@ -22,31 +21,18 @@ const slides = [
        <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
   </div>
  </div>,
-    <div className="flex-col h-full justify-items-center w-full">
-    <h2 className='font-bold p-10 underline decoration-sky-700 md:text-3xl'>QUE HACEMOS</h2>
-    <div className='w-full h-full md:flex md:justify-between p-20'>
-       <p className='font-serif md:w-1/2 w-full md:text-2xl text-sm mb-3'>Somos una empresa dedicada a la creación de tecnología de punta, con el objetivo de mejorar la calidad de vida de las personas.
-       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, debitis numquam blanditiis quisquam quaerat voluptate et voluptas eveniet molestias fuga eius! Reiciendis fugit, laudantium ipsa fugiat repudiandae quaerat quod dolor!
-       </p>
-       <div className="tenor-gif-embed md:h-1/2 md:w-1/4 w-full p-10" data-postid="9466370892182877823" data-share-method="host" data-aspect-ratio="1">
-         <a href="https://tenor.com/view/carregando-loading-gif-9466370892182877823">Carregando Loading GIF</a>
-         from <a href="https://tenor.com/search/carregando-gifs">Carregando GIFs</a>
-         </div> 
-         <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
-    </div>
-   </div>,
+   <TecnologiasDiapositiva/>,
    <div className="flex-col h-full justify-items-center w-full">
-   <h2 className='font-bold p-10 underline decoration-sky-700 md:text-3xl'>POR QUE CONFIAR EN NOSOTROS</h2>
-   <div className='w-full h-full md:flex md:justify-between p-20'>
-      <p className='font-serif md:w-1/2 w-full md:text-2xl text-sm mb-3'>Somos una empresa dedicada a la creación de tecnología de punta, con el objetivo de mejorar la calidad de vida de las personas.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, debitis numquam blanditiis quisquam quaerat voluptate et voluptas eveniet molestias fuga eius! Reiciendis fugit, laudantium ipsa fugiat repudiandae quaerat quod dolor!
+   <h2 className='font-bold p-10 underline decoration-sky-700 md:text-3xl text-center md:h-12 h-64'>POR QUE CONFIAR EN NOSOTROS</h2>
+   <div className='w-full h-full flex-col justify-items-center items-center md:p-20 p-10'>
+      <p className=' font-sans w-full md:text-2xl text-base md:mb-10 mb-20 text-center'>Ya que contamos con un equipo de profesionales altamente capacitados, con experiencia en el desarrollo de software y la implementación de tecnologías de vanguardia.
       </p>
-      <div className="tenor-gif-embed md:h-1/2 md:w-1/4 w-full p-10" data-postid="9466370892182877823" data-share-method="host" data-aspect-ratio="1">
-        <a href="https://tenor.com/view/carregando-loading-gif-9466370892182877823">Carregando Loading GIF</a>
-        from <a href="https://tenor.com/search/carregando-gifs">Carregando GIFs</a>
-        </div> 
-        <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+      <figure className='inline-block text-center '>
+      <img className=' object-cover md:h-1/2 h-1/4 md:w-50 w-1/2 m-auto rounded-full' src="../../../src/assets/trabajoEquipo.jpeg" alt="trabajo en equipo" />
+      <figcaption className=' md:text-sm text-xs text-gray-500'>Nuestro equipo trabajando</figcaption>
+      </figure>   
    </div>
+
   </div>,    
   ];
 root.render(
---
+++ src/tecnologias.jsx
@@ -0,0 +1,30 @@
+import './index.css';
+const TecnologiasDiapositiva = () => {
+    const tecnologias = [
+      { nombre: 'React', icono: '../../../src/assets/react_logo.svg' },
+      { nombre: 'Python', icono: '../../../src/assets/python_logo.svg' },
+      { nombre: 'Kotlin', icono: '../../../src/assets/kotlin_logo.svg' },
+      { nombre: 'Swift', icono: '../../../src/assets/swift_logo.svg' },
+    ];
+  
+    return (
+      <div className="flex-col justify-items-center text-center h-full w-full">
+        <h2 className="md:text-3xl font-bold mb-4 p-10 underline decoration-sky-700 md:h-11 h-64">QUE HACEMOS</h2>
+        <div className='flex-col md:justify-center md:gap-4 md:p-20 p-5 h-full'>
+            <p className="mb-6 md:text-2xl text-base md:h-1/4 h-1/3 font-sans">
+            Desarrollamos soluciones de software innovadoras utilizando las últimas tecnologías.
+            </p>
+            <div className="flex justify-center flex-wrap md:h-1/2 h-1/3">
+                {tecnologias.map((tec, index) => (
+                    <div key={index} className="flex flex-col items-center m-4">
+                        <img src={tec.icono} alt={tec.nombre} className="md:w-12 md:h-12 mb-2 w-6 h-6" />
+                        <span className='font-sans '>{tec.nombre}</span>
+                    </div>
+          ))}
+        </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default TecnologiasDiapositiva;
